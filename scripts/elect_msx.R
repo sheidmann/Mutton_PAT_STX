eed32a94835881da29bdf15d09cae24f6a7c2600
Sarah Heidmann <sarah.heidmann@gmail.com>
2024-06-10 10:55:23 -0400

    combine Chesson figures for DAFM and time of day for manuscript figure 4

--- scripts/elect_msx.R
+++ scripts/elect_msx.R
@@ -3,13 +3,14 @@
 # Sarah Heidmann
 
 # Created 9 Oct 2018
-# Last modified 30 March 2023
+# Last modified 1 Dec 2023
 
 # This script is based on Google searching 
 # As well as a script written by Daniel Holstein for MES 504
 # - edited by Sennai Habtes 3/26/2016 
 
 # In this script, focus on the electivity index using binned data
+# It contains the code for Figure 4 of the manuscript
 
 # Load libraries
 library(tidyverse)
@@ -198,8 +199,8 @@ freqHalfHourEIchi
 # Warning: chi-squared approximation may be incorrect
 # Not significant but chi-square doesn't really work on proportions.
 
-# Plot- manuscript figure 8
-spawnlim <- c(14.5,16)
+# Plot
+spawnlim <- c(12,15)
 ggplot() +
    # Highlight spawning time
    geom_vline(aes(xintercept=spawnlim[1]), linetype="dotted") +
@@ -217,7 +218,6 @@ ggplot() +
    theme(panel.background = element_blank(), legend.key = element_blank(),
          text = element_text(size = 20), axis.line = element_line(),
          legend.position = "top")
-#ggsave("outputs/Figure8_ChessonHalfHour_20230330.jpeg")
 
 ##### Chesson- presence/absence by month #####
 # Now by month
@@ -252,7 +252,7 @@ freqmonthAll <- freqmonthls %>%
    mutate(RdivP = prop / ChessonP) %>% 
    mutate(ChessonS = RdivP / sum(RdivP)) %>% 
    arrange(desc(ChessonS))
-freqmonthAllCast
+freqmonthAll
 # May and Apr are close, then Jun, then Mar then Jul
 
 ##### Chesson- presence absence by DAFM #####
@@ -305,6 +305,46 @@ dafmChesson <- ggline(freqDAFM_plot, x="DAFM",y="value",color = "type",
    scale_x_continuous(breaks = c(-10:20), labels = c(-10:20)) +
    theme(text = element_text(size = 18),
          axis.text.x = element_text(size = 12))
-# ggexport(dafmChesson,
-#          filename="outputs/Figure4_20230330.jpeg",
-#          width = 600)
+
+##### Combo DAFM/TimeOfDay manuscript figure #####
+# This is the code for Figure 4 of the manuscript
+# Code copied from above
+dafm_p <- ggline(freqDAFM_plot, x="DAFM",y="value",color = "type",
+       plot_type = "l", xlab="Days after full moon (DAFM)",
+       ylab="Chesson Election Index") +
+   scale_color_manual(name="", 
+                      values=c(ChessonS="mediumaquamarine",
+                               ChessonS_abs="orangered1",
+                               ChessonP="black"),
+                      breaks=c("ChessonS","ChessonS_abs","ChessonP"),
+                      labels=c("Present","Absent","Expected")) +
+   scale_x_continuous(breaks = c(-10:20), labels = c(-10:20)) +
+   theme(text = element_text(size = 18),
+         axis.text.x = element_text(size = 12))
+
+freqHalfHourEI <- freqHalfHourEI %>% 
+   mutate(mssca=factor(mssca,levels=c("out","in")))
+halfhour_p <- ggplot() +
+   # Highlight spawning time
+   geom_vline(aes(xintercept=spawnlim[1]), linetype="dotted") +
+   geom_vline(aes(xintercept=spawnlim[2]), linetype="dotted") +
+   geom_rect(aes(xmin=spawnlim[1], xmax = spawnlim[2], 
+                 ymin=-Inf,ymax = Inf), fill = "gray90") +
+   # The main event
+   geom_line(aes(x=halfhour,y=ChessonS,color=mssca), data = freqHalfHourEI) +
+   # Show expected Chesson value
+   geom_hline(yintercept = 1/nrow(freqHalfHourEI), color = "black") +
+   # Formatting
+   scale_x_continuous(breaks = c(0:23), labels = c(0:23)) +
+   scale_color_discrete(name= "MSSCA") +
+   xlab("Time of Day") +
+   ylab("Chesson Election Index") +
+   theme(panel.background = element_blank(), legend.key = element_blank(),
+         text = element_text(size = 20), axis.line = element_line(),
+         legend.position = "top")
+
+chesson_figure <- ggarrange(dafm_p, halfhour_p, labels = c("A","B"), ncol=1)
+# ggexport(chesson_figure, filename = "outputs/chesson_figure_20231201.jpeg",
+#          width = 900, height = 1000)
+# ggexport(chesson_figure, filename = "outputs/Fig4_chesson_202404.tiff", res=300,
+#          width = 2900, height = 3000)
